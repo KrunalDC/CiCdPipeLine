4d36bdc432a1b70affa042b0127d4bd7d8a882c5
krunalDC <krunal.patel6@dcmail.ca>
2024-08-03 17:53:07 -0400

    changes in test file

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import pytest
-from app import create_app
+from app import app
 
 @pytest.fixture
 def client():
-    app = create_app()
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
@@ -12,4 +11,4 @@ def test_homepage(client):
     """Test the homepage"""
     rv = client.get('/')
     assert rv.status_code == 200
-    assert b"Welcome" in rv.data
+    assert b"Hello, World!" in rv.data
